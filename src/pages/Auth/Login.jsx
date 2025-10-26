ddd6a0134b6b45e5218b02461ac7ef49cd574cfd
Igor <igorsvk16@gmail.com>
2025-10-26 23:49:33 +0300

    написание авторизации

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
 import styles from './Auth.module.css'
 
 export default function Login() {
+    const [form, setForm] = useState({ login: '', password: ''});
+    const [error, setError] = useState('');
+    
+    function handleSubmit(e) {
+      const { name, value } = e.target;
+    }
+    async function handleSubmit(e) {
+      e.preventDefault();
+    }
+
     return (
-        <div className={styles.container}>
-        </div>
+        <section className={styles.container}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+  <h1>Вход</h1>
+  <label>
+    Логин
+    <input name="login" value={form.login} onChange={handleChange} />
+  </label>
+  <label>
+    Пароль
+    <input type="password" name="password" value={form.password} onChange={handleChange} />
+  </label>
+  {error && <p className={styles.error}>{error}</p>}
+  <button type='submit'>Войти</button>
+  <p className={styles.switch}>
+    Нет аккаунта? <Link to="/auth/register">Создать</Link>
+  </p>
+</form>
+        </section>
     );
 }
