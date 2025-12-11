767e8ea8676dc042cb68a79375f20dae1d5504ff
Igor <igorsvk16@gmail.com>
2025-12-11 11:40:02 +0300

    deleted demo data

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ProjectsList from './pages/Projects/ProjectsList';
 import NewProject from './pages/Projects/NewProject';
-import ProjectView from './pages/Projects/ProjectView';
-import ProjectViewV2 from './pages/Projects/ProjectViewV2';
-import ProjectViewDetailed from './pages/Projects/ProjectViewDetailed';
-import ProjectViewStream from './pages/Projects/ProjectViewStream';
 import ProjectAnalysis from './pages/Projects/ProjectAnalysis';
-import ProjectViewLayered from './pages/Projects/ProjectViewLayered';
 import TestFlow from './pages/Projects/TestFlow';
 import Settings from './pages/Settings/Settings';
 import ProtectedRoute from './routes/ProtectedRoute';
@@ -50,50 +45,10 @@ export default function App() {
             }
           />
           <Route
-            path="/projects/:id/basic"
-            element={
-              <ProtectedRoute>
-                <ProjectView />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/projects/:id/v2"
-            element={
-              <ProtectedRoute>
-                <ProjectViewV2 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/projects/:id/detailed"
-            element={
-              <ProtectedRoute>
-                <ProjectViewDetailed />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/projects/:id/stream"
-            element={
-              <ProtectedRoute>
-                <ProjectViewStream />
-              </ProtectedRoute>
-            }
-          />
-          <Route
             path="/projects/:id/architecture"
             element={
               <ProtectedRoute>
                 <ProjectAnalysis />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/projects/:id/layered"
-            element={
-              <ProtectedRoute>
-                <ProjectViewLayered />
               </ProtectedRoute>
             }
           />
