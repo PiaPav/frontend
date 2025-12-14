eb9190df05012e29342f57511ae4bc497633a779
Igor <igorsvk16@gmail.com>
2025-12-14 18:30:14 +0300

    add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import deepLearningFavicon from './assets/img/logo/deep-learning.png'
+
+const faviconElement =
+  document.querySelector("link[rel~='icon']") || document.createElement('link')
+faviconElement.rel = 'icon'
+faviconElement.type = 'image/png'
+faviconElement.href = deepLearningFavicon
+if (!faviconElement.parentNode) {
+  document.head.appendChild(faviconElement)
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
